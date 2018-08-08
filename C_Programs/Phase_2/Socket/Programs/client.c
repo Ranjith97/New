bf03df7cf43b6e93a3158be5b7eb48917b0c189b
Ranjith97 <ranjithkumar.kv@vvdntech.in>
2018-08-08 22:06:29 +0530

    socket

---
+++ C_Programs/Phase_2/Socket/Programs/client.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<arpa/inet.h>
+#include<string.h>
+#define PORT 1200
+
+
+char buffer[1024] = "Hei all";
+char buffer1[1024] = "Hei all";
+
+char hello[100] = "Hello";
+void* sendThread (void* arg) {
+	int sock = *(int *)arg;
+	int isFirst = 1;
+	while(1) {
+		strcpy(hello,"");
+		if(isFirst == 0) {
+			getchar();
+		}
+		isFirst = 0;
+		scanf("%[^\n]s",hello);
+		write(sock,hello,htonl(strlen(hello)));
+		hello[0] = '\0';
+	}
+	pthread_exit(0);
+
+}
+
+void* listenThread (void* arg) {
+	int sock = *(int *)arg;
+	while(1) {
+
+		strcpy(buffer,"");
+		read( sock , buffer, 1024);
+		sleep(1);
+		printf("%s\n",buffer );
+	}
+	pthread_exit(0);
+}
+
+int main(int argc, char const *argv[])
+{
+	int sock = 0;
+	pthread_t tid1,tid2;
+	struct sockaddr_in serv_addr;
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	pthread_create(&tid1,NULL,listenThread,&sock);
+	pthread_create(&tid2,NULL,sendThread,&sock);
+	pthread_join(tid1,NULL);
+	pthread_join(tid2,NULL);
+	return 0;
+}
---
+++ C_Programs/Phase_2/Socket/Programs/server.c
@@ -0,0 +1,100 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<pthread.h>
+#include<string.h>
+
+#define PORT 1200
+
+char buffer[1024] = "";
+
+int flag =0;
+char currClient[3] = "";
+
+void* clients(void *socket) {
+
+	int new_socket = *(int *)socket;
+	flag = 1;
+
+
+	while(1) {
+
+		send(new_socket , buffer , htonl(strlen(buffer)) , 0 );
+		sleep(1);
+		flag = 0;
+
+	}
+
+	buffer[0] = '\0';
+	printf("Hello message sent\n");
+	pthread_exit(0);
+}
+
+void* clientListen(void *socket) {
+	int new_socket = *(int *)socket;
+	while(1) {
+		read( new_socket , buffer, 1024);
+
+		sprintf(currClient,"%d",new_socket);
+		currClient[strlen(currClient)] = ':';
+
+
+		sleep(1);
+        memset(buffer, 0, 1024);
+		flag = 0;
+	}
+	pthread_exit(0);
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int server_fd, new_socket;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	pthread_t thread_id[10][2];
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+				&opt, sizeof(opt)))
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
+
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr *)&address,
+				sizeof(address))<0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	int i = 0;
+	while(1) {
+		if (listen(server_fd, 3) < 0)
+		{
+			perror("listen");
+			exit(EXIT_FAILURE);
+		}
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+						(socklen_t*)&addrlen))<0)
+		{
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+		pthread_create(&thread_id[i++][0],NULL,clients,&new_socket);
+		pthread_create(&thread_id[i++][1],NULL,clientListen,&new_socket);
+
+	}
+	return 0;
+}
